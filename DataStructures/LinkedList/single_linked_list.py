89f97d55fa6cb8f206e811d253494c1817e1cf9c
Manish7031 <singh.7031@gmail.com>
2026-01-28 22:50:37 +0530

    DS commit 1.4

--- DataStructures/LinkedList/single_linked_list.py
+++ DataStructures/LinkedList/single_linked_list.py
@@ -15,8 +15,29 @@ class LinkedList:
         else:
             sh = self.head
             while sh is not None:
-                print(sh.data)
+                print(sh.data, "-->", end=" ")
                 sh = sh.ref
+    
+    def add_nodeAtBegin(self, data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+    
+    def add_nodeAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            sh = self.head
+            while sh.ref is not None:
+                sh = sh.ref
+            sh.ref = new_node
+# Example usage:
 
 ll1 = LinkedList()
+ll1.add_nodeAtBegin(10)
+ll1.add_nodeAtBegin(20)
+ll1.add_nodeAtBegin(30)
+ll1.add_nodeAtEnd(40)
+ll1.add_nodeAtEnd(50)
 ll1.print_LL()
